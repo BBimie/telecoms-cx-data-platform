e9a79bad2ef61bbbfcfca25e43a6ce1c2f71a9fc
BBimie <babatolatemi@gmail.com>
2025-11-25 22:51:34 +0100

    file for testing scripts

---
+++ main.py
@@ -0,0 +1,10 @@
+from scripts.extract.agents import extract_agents_data
+from scripts.extract.customers import extract_customer_data
+from scripts.extract.call_center_logs import extract_call_center_logs, delete_folder
+
+
+# extract_customer_data() = (2499948, 7)
+
+# extract_call_center_logs()
+
+# extract_agents_data()
