af3d6bf152f2c22d06348ea0758bb6f971d8dbe2
BBimie <babatolatemi@gmail.com>
2025-11-30 01:31:34 +0100

    added database to env

--- scripts/load/load_raw_to_snowflake.py
+++ scripts/load/load_raw_to_snowflake.py
@@ -51,5 +51,5 @@ if __name__ == "__main__":
     for table, folder in mappings.items():
         copy_data_from_s3_to_snowflake(table, 
                                        folder, 
-                                       database='CORE_TELECOMS', 
+                                       database=Constant.CORETELECOMS_DATABASE, 
                                        schema='RAW')
