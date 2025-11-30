2bb0346654864d949d47a0114667db128a18fbca
BBimie <babatolatemi@gmail.com>
2025-11-30 01:17:39 +0100

    Added dbt staging models

---
+++ dbt/dbt_core_telecoms/models/staging/stg_call_center_logs.sql
@@ -0,0 +1,24 @@
+WITH source AS (
+    SELECT * FROM {{ source('raw_data', 'CALL_CENTER_LOGS') }}
+),
+
+renamed AS (
+    SELECT
+        data:"call ID"::varchar(50) AS call_id,
+        data:"Unnamed 0"::bigint AS call_index,
+        data:"customeR iD"::varchar(50) AS customer_id,
+        data:"agent ID"::int AS agent_id,
+        data:"COMPLAINT_catego ry"::varchar(100) AS complaint_topic,
+        data:resolutionstatus::varchar(50) AS resolution_status,
+        data:call_start_time::timestamp AS call_start_time,
+        data:call_end_time::timestamp AS call_end_time,
+        DATEDIFF(second, data:call_start_time::timestamp, data:call_end_time::timestamp) AS duration_seconds,
+        data:callLogsGenerationDate::date AS logs_generation_date,
+        data:_source_file::varchar(100) AS source_filename,
+        TO_TIMESTAMP_NTZ(data:"_data_load_time"::bigint, 6) AS source_extracted_at,
+        load_time AS snowflake_loaded_at
+
+    FROM source
+)
+
+SELECT * FROM renamed
