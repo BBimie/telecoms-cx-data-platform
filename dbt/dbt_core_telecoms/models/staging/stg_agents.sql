c7a4956a57de0b99c76d074e78466c43597e8e2b
BBimie <babatolatemi@gmail.com>
2025-12-26 20:51:28 +0100

    Updated dbt staging model scripts

--- dbt/dbt_core_telecoms/models/staging/stg_agents.sql
+++ dbt/dbt_core_telecoms/models/staging/stg_agents.sql
@@ -1,8 +1,4 @@
-WITH source AS (
-    SELECT * FROM {{ source('raw_data', 'AGENTS') }}
-),
 
-renamed AS (
     SELECT
         "data":iD::int AS agent_id,
         "data":NamE::varchar(100) AS agent_name,
@@ -10,7 +6,4 @@ renamed AS (
         "data":state::varchar(50) as state,
         TO_TIMESTAMP_NTZ("data":"_data_load_time"::bigint, 6) AS source_extracted_at,
         "snowflake_load_time" AS snowflake_loaded_at
-    FROM source
-)
-
-SELECT * FROM renamed
+    FROM {{ source('raw_data', 'AGENTS') }} ;
--- dbt/dbt_core_telecoms/models/staging/stg_customers.sql
+++ dbt/dbt_core_telecoms/models/staging/stg_customers.sql
@@ -1,35 +1,28 @@
-WITH source AS (
-    SELECT * FROM {{ source('raw_data', 'CUSTOMERS') }}
-),
 
-renamed AS (
-    SELECT
-        "data":customer_id::varchar(50) AS customer_id,
-        "data":name::varchar(100) AS customer_name,
+SELECT
+    "data":customer_id::varchar(50) AS customer_id,
+    "data":name::varchar(100) AS customer_name,
 
-        -- CLEAN EMAIL FIELD
-        CASE 
-            -- Fix Gmail typos gmail.om > gmail.com
-            WHEN LOWER("data":email::varchar(100)) LIKE '%@gmail.om' 
-                THEN REGEXP_REPLACE(LOWER("data":email::varchar(100)), '@gmail.om$', '@gmail.com')
-            
-            -- Fix Hotmail typos hotmai.com > hotmail.com
-            WHEN LOWER("data":email::varchar(100)) LIKE '%@hotmai.com' 
-                THEN REGEXP_REPLACE(LOWER("data":email::varchar(100)), '@hotmai.com$', '@hotmail.com')
-            
-            -- Catch-all for other ".om" endings
-            WHEN LOWER("data":email::varchar(100)) LIKE '%.om' 
-                THEN REGEXP_REPLACE(LOWER("data":email::varchar(100)), '\.om$', '.com')
+    -- CLEAN EMAIL FIELD
+    CASE 
+        -- Fix Gmail typos gmail.om > gmail.com
+        WHEN LOWER("data":email::varchar(100)) LIKE '%@gmail.om' 
+            THEN REGEXP_REPLACE(LOWER("data":email::varchar(100)), '@gmail.om$', '@gmail.com')
+        
+        -- Fix Hotmail typos hotmai.com > hotmail.com
+        WHEN LOWER("data":email::varchar(100)) LIKE '%@hotmai.com' 
+            THEN REGEXP_REPLACE(LOWER("data":email::varchar(100)), '@hotmai.com$', '@hotmail.com')
+        
+        -- Catch-all for other ".om" endings
+        WHEN LOWER("data":email::varchar(100)) LIKE '%.om' 
+            THEN REGEXP_REPLACE(LOWER("data":email::varchar(100)), '\.om$', '.com')
 
-            ELSE LOWER("data":email::varchar(100))
-        END AS email,
-        "data":address::varchar(255) AS address,
-        "data":Gender::varchar(10) AS gender,
-        "data":"DATE of biRTH"::date AS date_of_birth,
-        "data":signup_date::date AS signup_date,
-        TO_TIMESTAMP_NTZ("data":"_data_load_time"::bigint, 6) AS source_extracted_at,
-        "snowflake_load_time" AS snowflake_loaded_at
-    FROM source
-)
-
-SELECT * FROM renamed
+        ELSE LOWER("data":email::varchar(100))
+    END AS email,
+    "data":address::varchar(255) AS address,
+    "data":Gender::varchar(10) AS gender,
+    "data":"DATE of biRTH"::date AS date_of_birth,
+    "data":signup_date::date AS signup_date,
+    TO_TIMESTAMP_NTZ("data":"_data_load_time"::bigint, 6) AS source_extracted_at,
+    "snowflake_load_time" AS snowflake_loaded_at
+FROM {{ source('raw_data', 'CUSTOMERS') }} ;
